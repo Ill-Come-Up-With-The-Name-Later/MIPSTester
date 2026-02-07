730924e032090c1d445129b6a00ac7041fe3917b
Ill-Come-Up-With-The-Name-Later <adtmax5@gmail.com>
2026-02-07 14:06:45 -0500

    Changed argument for file running

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ import misc.Memory;
 import program.Program;
 import util.BinaryConversion;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-		FileParser.GLOBAL.readFile("program.asm");
+		FileParser.GLOBAL.readFile(args[0]);
 		Program.MAIN_PROGRAM.run();
 
 		System.out.println("Post-execution");
