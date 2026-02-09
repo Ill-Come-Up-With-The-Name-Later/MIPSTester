c72ec614b985bc1ed41e0ceee282a2dc7c3913dd
Ill-Come-Up-With-The-Name-Later <adtmax5@gmail.com>
2026-02-09 06:27:38 -0500

    Comment formatting

--- src/util/MathHelper.java
+++ src/util/MathHelper.java
@@ -5,6 +5,7 @@ public class MathHelper {
 	/**
 	 * Multiplies two 32-bit integers. Accounts
 	 * for the possibility of creating a 64-bit result.
+	 *
 	 * @param a A number
 	 * @param b A number
 	 * @return The result of multiplication
